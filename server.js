c8e78b9e8843e37520f2190a556ae37a7ea2ab67
Nick <nicknewburg@gmail.com>
2019-11-20 18:01:20 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
